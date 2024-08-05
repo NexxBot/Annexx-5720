92d17800ecd36c8bf3eeb05994b77c7a2e64269c
Perye LI <lipengyu.cn@hotmail.com>
2024-08-05 16:57:08 +0800

    Initial commit

---
+++ annexx/__init__.py
@@ -0,0 +1,6 @@
+# ./venv/bin/python
+# _*_ coding: utf-8 _*_
+# @Time     : 2024/8/5 16:18
+# @Author   : Perye (Pengyu) LI
+# @FileName : __init__.py.py
+# @Software : PyCharm
---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+# ./venv/bin/python
+# _*_ coding: utf-8 _*_
+# @Time     : 2024/8/5 16:19
+# @Author   : Perye (Pengyu) LI
+# @FileName : __init__.py.py
+# @Software : PyCharm
